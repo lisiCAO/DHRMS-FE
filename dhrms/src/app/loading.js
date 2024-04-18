fcad31f59896106717e9bfa0e2da19f4b0bcdc58
lisiCAO <caolisi@hotmail.com>
2024-04-18 00:44:45 -0400

    update loading js

--- dhrms/src/app/loading.js
+++ dhrms/src/app/loading.js
@@ -1,12 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import LinearProgress from '@mui/material/LinearProgress';
+import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import LinearProgressWithLabel from '@mui/material/LinearProgress';
 
-const Loading = () => {
-    return (
-      <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '100vh' }}>
-        <LinearProgressWithLabel />
+function LinearProgressWithLabel({ value }) {
+  return (
+    <Box sx={{ display: 'flex', alignItems: 'center' }}>
+      <Box sx={{ width: '100%', mr: 1 }}>
+        <LinearProgress variant="determinate" value={value} />
       </Box>
-    );
-  };
-  
-  export default Loading;
+      <Box sx={{ minWidth: 35 }}>
+        <Typography variant="body2" color="text.secondary">{`${Math.round(value)}%`}</Typography>
+      </Box>
+    </Box>
+  );
+}
+
+LinearProgressWithLabel.propTypes = {
+  value: PropTypes.number.isRequired,
+};
+
+const Loading = ({ value }) => {
+  return (
+    <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '100vh' }}>
+      <LinearProgressWithLabel value={value} />
+    </Box>
+  );
+};
+
+Loading.propTypes = {
+  value: PropTypes.number.isRequired,
+};
+
+export default Loading;
