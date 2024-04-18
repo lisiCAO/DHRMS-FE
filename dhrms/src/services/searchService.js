60d32468ae4595d2cb4912a2b166ff1a578799af
lisiCAO <caolisi@hotmail.com>
2024-04-18 00:01:16 -0400

    config security api

--- dhrms/src/services/searchService.js
+++ dhrms/src/services/searchService.js
@@ -1,7 +1,7 @@
 import apiClient from '@/utils/apiClient';
 
 export const fetchSearchResults = (searchTerm) => {
-  return apiClient.get(`/search?query=${encodeURIComponent(searchTerm)}`)
+  return apiClient.get(`/search`, { params: { query: searchTerm } })
     .then(response => response.data)
     .catch(error => {
       throw error;
