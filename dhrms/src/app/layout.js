b492cc465895854e66f7dfbc7c8475a95d568329
lisiCAO <caolisi@hotmail.com>
2024-04-18 19:49:23 -0400

    adjust laytout

--- dhrms/src/app/layout.js
+++ dhrms/src/app/layout.js
@@ -2,14 +2,15 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 
-import React, { useState } from "react";
+import React, { Suspense, useState } from "react";
 import Head from "next/head";
-import { AppBar, Toolbar, IconButton, Typography } from "@mui/material";
+import { AppBar,Container, Box, Toolbar, IconButton, Typography } from "@mui/material";
 import { Menu as MenuIcon, Brightness4, Language } from "@mui/icons-material";
 import Footer from "@/components/Footer";
 
 import { ThemeProvider } from "@/contexts/ThemeContext";
 import { LanguageProvider } from "@/contexts/LanguageContext";
+import Loading from "./loading";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,49 +25,55 @@ export default function RootLayout({ children }) {
         <meta name='description' content='TODO' />
       </Head>
       <body className={inter.className}>
-      <ThemeProvider>
-        <LanguageProvider>
-          <div className={theme === "dark" ? "dark-theme" : "light-theme"}>
-            <AppBar
-              position="fixed"
-              color='default'
-              elevation={0}
-              className='shadow-none'>
-              <Toolbar
-                style={{ display: "flex", justifyContent: "space-between" }}>
-                <IconButton
-                  edge='start'
-                  color='inherit'
-                  aria-label='menu'
-                  onClick={() => setModalOpen(true)}>
-                  <MenuIcon />
-                </IconButton>
-                <Typography
-                  variant='h6'
-                  style={{ textAlign: "center", flexGrow: 1 }}>
-                  QLMS
-                </Typography>
-                <div>
+        <ThemeProvider>
+          <LanguageProvider>
+            <div className={theme === "dark" ? "dark-theme" : "light-theme"}>
+            <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+              <AppBar
+                position="fixed"
+                color='default'
+                elevation={0}
+                className='shadow-none'>
+                <Toolbar
+                  style={{ display: "flex", justifyContent: "space-between" }}>
                   <IconButton
+                    edge='start'
                     color='inherit'
-                    onClick={() =>
-                      setTheme(theme === "light" ? "dark" : "light")
-                    }>
-                    <Brightness4 />
+                    aria-label='menu'
+                    onClick={() => setModalOpen(true)}>
+                    <MenuIcon />
                   </IconButton>
-                  <IconButton color='inherit'>
-                    <Language />
-                  </IconButton>
-                </div>
-              </Toolbar>
-            </AppBar>
+                  <Typography
+                    variant='h6'
+                    style={{ textAlign: "center", flexGrow: 1 }}>
+                    QLMS
+                  </Typography>
+                  <div>
+                    <IconButton
+                      color='inherit'
+                      onClick={() =>
+                        setTheme(theme === "light" ? "dark" : "light")
+                      }>
+                      <Brightness4 />
+                    </IconButton>
+                    <IconButton color='inherit'>
+                      <Language />
+                    </IconButton>
+                  </div>
+                </Toolbar>
+              </AppBar>
 
-            <main>{children}</main>
-            <Footer />
-          </div>
-        </LanguageProvider>
-      </ThemeProvider>
-        </body>
+                <Suspense fallback={<Loading />}>
+                <Container component="main" sx={{ flexGrow: 1, mt: 8, mb: 2 }}>
+                  {children}
+                </Container>
+                </Suspense>
+              <Footer />
+              </Box>
+            </div>
+          </LanguageProvider>
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
--- dhrms/src/app/map/page.js
+++ dhrms/src/app/map/page.js
@@ -1,45 +1,42 @@
 "use client";
 import React, { useState, useEffect } from 'react';
-import { useRouter } from 'next/navigation';
+import { useSearchParams } from 'next/navigation';
 import axios from 'axios';
 import GoogleMapComponent from '@/components/GoogleMapComponent'; 
-import Loading from '@/components/Loading';  
+import { useLoadScript } from '@react-google-maps/api';
+import ErrorComponent from '@/components/ErrorComponent';
 
 const SearchPage = () => {
-    const router = useRouter();
+    const searchParams = useSearchParams()
     const [results, setResults] = useState([]);
-    const [loading, setLoading] = useState(false);
-    const [progress, setProgress] = useState(0);
+    const [error, setError] = useState('');
+
+    const { isLoaded, loadError } = useLoadScript({
+        googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+        version: "weekly"
+    });
 
     useEffect(() => {
-        const searchTerm = router.query.search;
-        if (searchTerm) {
-            setLoading(true);
-            setProgress(10); 
- 
-            const interval = setInterval(() => {
-                setProgress(prevProgress => prevProgress < 100 ? prevProgress + 20 : 100);
-            }, 1000);
+        const searchTerm = searchParams.get('search');
+        if (!searchTerm) return;
+        axios.get(`/api/search?query=${encodeURIComponent(searchTerm)}`)
+            .then(response => {
+                setResults(response.data);
+                setError('');
+            })
+            .catch(error => {
+                console.error('Search error:', error);
+                setError('Failed to fetch results. Please try again.');
+            });
+    }, [searchParams]);
 
-            axios.get(`/api/search?query=${encodeURIComponent(searchTerm)}`)
-                .then(response => {
-                    setResults(response.data);
-                    setProgress(100);  
-                })
-                .catch(error => {
-                    console.error('Search error:', error);
-                    setProgress(100);  
-                })
-                .finally(() => {
-                    setLoading(false);
-                    clearInterval(interval); 
-                });
-        }
-    }, [router.query.search]);
+    if (loadError) return <ErrorComponent message="Error loading map" />;
+    if (!isLoaded) return <div>Loading map...</div>;
+    if (error) return <ErrorComponent message={error} />;
 
     return (
-        <div>
-            {loading ? <Loading value={progress} /> : <GoogleMapComponent results={results} />}
+        <div className=''>
+            <GoogleMapComponent results={results} />
         </div>
     );
 };
