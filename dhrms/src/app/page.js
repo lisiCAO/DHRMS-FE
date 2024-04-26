0b8cbb2d32103a953c4e13808d9f75ed2dec93ee
lisiCAO <caolisi@hotmail.com>
2024-04-26 05:48:03 -0400

    home page

--- dhrms/src/app/page.js
+++ dhrms/src/app/page.js
@@ -1,9 +1,17 @@
+import { Typography } from "@mui/material";
 import Image from "next/image";
+
 
 export default function Home() {
   return (
-    <p>
-      Welcome to Home Page
-    </p>
+    <>
+      <Typography variant="h2" component="h1" gutterBottom>
+        Welcome to DHRMS
+      </Typography>
+      <Typography variant="h5">
+        Join us in our mission to make the world a better place.
+      </Typography>
+      <Typography variant="h5">Team3: DHRMS</Typography>
+    </>
   );
 }
